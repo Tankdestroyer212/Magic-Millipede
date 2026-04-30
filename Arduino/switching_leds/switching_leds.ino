e4b13fb377caa5c9e416e1bea5ca37e3c0bccb6c
Dhruv Kagatimath <dhruvkagatimath@gmail.com>
2026-04-30 07:06:16 -0700

    Changed the logic to make sure the button can reset the lights for another cycle

--- Arduino/switching_leds/switching_leds.ino
+++ Arduino/switching_leds/switching_leds.ino
@@ -2,17 +2,13 @@
 #include <SoftwareSerial.h>
 #include <DFRobotDFPlayerMini.h>
 
-#define EXT_LED_PIN   6
-#define INT_LED_PIN   5
-#define NUM_LEDS      7
-#define BUTTON_PIN    7
-#define UV_RELAY_PIN  8
-#define MP3_RX        3
-#define MP3_TX        2
-
-#define PHASE1_END  30000    // 0:30
-#define PHASE2_END  90000    // 1:30
-#define PHASE3_END  120000   // 2:00
+#define EXT_LED_PIN  6
+#define INT_LED_PIN  5
+#define NUM_LEDS     7
+#define BUTTON_PIN   7
+#define UV_RELAY_PIN 8
+#define MP3_RX       3
+#define MP3_TX       2
 
 Adafruit_NeoPixel exterior(NUM_LEDS, EXT_LED_PIN, NEO_RGB + NEO_KHZ800);
 Adafruit_NeoPixel interior(NUM_LEDS, INT_LED_PIN, NEO_RGB + NEO_KHZ800);
@@ -20,17 +16,13 @@ Adafruit_NeoPixel interior(NUM_LEDS, INT_LED_PIN, NEO_RGB + NEO_KHZ800);
 SoftwareSerial mp3Serial(MP3_RX, MP3_TX);
 DFRobotDFPlayerMini player;
 
-enum State {
-  DEFAULT_STATE,
-  PRESENTATION_PHASE1,
-  PRESENTATION_PHASE2,
-  PRESENTATION_PHASE3
-};
+#define DEFAULT    0
+#define PHASE1  1
+#define PHASE2  2
+#define PHASE3  3
 
-State currentState = DEFAULT_STATE;
-
-unsigned long presentationStartTime = 0;
-bool lastButtonState = HIGH;
+int currentState = DEFAULT;
+unsigned long phaseStart = 0;
 
 void setColor(Adafruit_NeoPixel &ring, int r, int g, int b) {
   for (int i = 0; i < NUM_LEDS; i++) {
@@ -39,89 +31,85 @@ void setColor(Adafruit_NeoPixel &ring, int r, int g, int b) {
   ring.show();
 }
 
-void uvOn()  { digitalWrite(UV_RELAY_PIN, HIGH); }
-void uvOff() { digitalWrite(UV_RELAY_PIN, LOW);  }
+void uvOn()  { digitalWrite(UV_RELAY_PIN, LOW);  }
+void uvOff() { digitalWrite(UV_RELAY_PIN, HIGH); }
 
-void enterDefaultState() {
-  currentState = DEFAULT_STATE;
-  presentationStartTime = 0;
-  uvOff();
-  setColor(exterior, 255, 0, 0); // Exterior green
-  setColor(interior, 0, 255, 0); // Interior red
+void triggerAudio(int track) {
+  delay(100);
+  player.play(track);
+  delay(100);
+}
+
+void resetAudio() {
   player.stop();
-  lastButtonState = HIGH;
-}
-
-void enterPhase1() {
-  currentState = PRESENTATION_PHASE1;
-  presentationStartTime = millis();
-  uvOff();
-  setColor(exterior, 0, 255, 0); // Exterior red (room occupied)
-  setColor(interior, 0, 255, 0); // Interior red stays ON
   delay(500);
-  player.play(1);
-}
-
-void enterPhase2() {
-  currentState = PRESENTATION_PHASE2;
-  uvOn();
-  setColor(interior, 0, 0, 0);
-}
-
-void enterPhase3() {
-  currentState = PRESENTATION_PHASE3;
-  uvOff();
-  setColor(interior, 0, 0, 0);
 }
 
 void setup() {
+  Serial.begin(9600);
+
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(UV_RELAY_PIN, OUTPUT);  // was missing!
-  uvOff();                         // was missing!
+  pinMode(UV_RELAY_PIN, OUTPUT);
+  uvOff();
 
   exterior.begin();
   exterior.show();
+  setColor(exterior, 255, 0, 0); // exterior green
+
   interior.begin();
   interior.show();
+  setColor(interior, 0, 255, 0); // interior red
 
+  delay(1000);
   mp3Serial.begin(9600);
   delay(1000);
   if (player.begin(mp3Serial)) {
+    Serial.println("DFPlayer connected!");
     player.volume(20);
+  } else {
+    Serial.println("DFPlayer failed");
   }
-
-  enterDefaultState();
 }
 
 void loop() {
-  bool currentButtonState = digitalRead(BUTTON_PIN);
-
   switch (currentState) {
 
-    case DEFAULT_STATE:
-      if (currentButtonState == LOW && lastButtonState == HIGH) {
-        enterPhase1();
+    case DEFAULT:
+      if (digitalRead(BUTTON_PIN) == LOW) {
+        currentState = PHASE1;
+        phaseStart = millis();
+        setColor(exterior, 0, 255, 0); // exterior red
+        setColor(interior, 0, 255, 0); // interior red stays on
+        triggerAudio(1);               // play track 1
       }
       break;
 
-    case PRESENTATION_PHASE1:
-    case PRESENTATION_PHASE2:
-    case PRESENTATION_PHASE3: {
-      // Only calculate elapsed when inside a presentation
-      unsigned long elapsed = millis() - presentationStartTime;
-
-      if (currentState == PRESENTATION_PHASE1 && elapsed >= PHASE1_END) {
-        enterPhase2();
-      }
-      else if (currentState == PRESENTATION_PHASE2 && elapsed >= PHASE2_END) {
-        enterPhase3();
-      }
-      else if (currentState == PRESENTATION_PHASE3 && elapsed >= PHASE3_END) {
-        enterDefaultState();
+    case PHASE1:
+      if (millis() - phaseStart >= 30000) {
+        currentState = PHASE2;
+        phaseStart = millis();
+        uvOn();                        // UV on
+        setColor(interior, 0, 0, 0);  // interior off
       }
       break;
-    }
+
+    case PHASE2:
+      if (millis() - phaseStart >= 60000) {
+        currentState = PHASE3;
+        phaseStart = millis();
+        uvOff();                       // UV off
+        setColor(interior, 0, 0, 0);  // room stays dark
+      }
+      break;
+
+    case PHASE3:
+      if (millis() - phaseStart >= 30000) {
+        currentState = DEFAULT;
+        uvOff();
+        resetAudio();                  // reset audio for next cycle
+        setColor(exterior, 255, 0, 0); // exterior green
+        setColor(interior, 0, 255, 0); // interior red
+      }
+      break;
   }
-
-  lastButtonState = currentButtonState;
 }
